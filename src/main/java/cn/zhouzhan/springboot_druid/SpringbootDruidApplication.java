ffc0fbbd993ce3130ac36119650d0bbdb14c052f
zhouzhan <hello_zhouzhan@163.com>
2019-08-07 23:17:06 +0800

    提交

---
+++ src/main/java/cn/zhouzhan/springboot_druid/SpringbootDruidApplication.java
@@ -0,0 +1,13 @@
+package cn.zhouzhan.springboot_druid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDruidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDruidApplication.class, args);
+    }
+
+}
