07c8eedd73b3ba90f938092f3a2e6374244911a1
zhouzhan <hello_zhouzhan@163.com>
2019-08-07 23:34:59 +0800

    22

--- src/main/java/cn/zhouzhan/springboot_druid/SpringbootDruidApplication.java
+++ src/main/java/cn/zhouzhan/springboot_druid/SpringbootDruidApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootDruidApplication {
 
+    //ahhaha
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDruidApplication.class, args);
     }
